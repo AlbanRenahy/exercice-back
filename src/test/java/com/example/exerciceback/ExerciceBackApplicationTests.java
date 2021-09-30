dfd7e331288ca4e3a48739c1595fc4f08d92e401
AlbanRenahy <albanrenahy.pro@gmail.com>
2021-09-30 15:15:38 +0200

    Create basic architecture

---
+++ src/test/java/com/example/exerciceback/ExerciceBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exerciceback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciceBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
